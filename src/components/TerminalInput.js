763932ee6374cdd56a28e67222017f4d576bcec5
Luke Hedgecox <luke@emotion-robotics.com>
2020-01-18 11:41:50 +0000

    Terminal Colors working
    Todo: Prevent setting the same forground and background color
    Add time stamps to ensure keys are unique on terminal inputs

--- src/components/TerminalInput.js
+++ src/components/TerminalInput.js
@@ -10,13 +10,13 @@ const TerminalInput = (props) => {
 
         tInput.current.focus();
         window.addEventListener('click', ButtonClick)
-    },[])
+    }, [])
 
-    
+
     const FormSubmit = (e) => {
         e.preventDefault();
-        if(inputText){ //Only update if input text is valid
-            props.setLines(lines => [...lines, {text: inputText, by: 1}]);
+        if (inputText) { //Only update if input text is valid
+            props.setLines(lines => [...lines, { text: inputText, by: 1 }]);
             computer(inputText, props.setLines)
             setInputText("");
         }
@@ -25,7 +25,7 @@ const TerminalInput = (props) => {
     return (
         <form onSubmit={FormSubmit}>
             <span>C:\></span>
-            <input onChange={e => setInputText(e.target.value)} value={inputText} ref={tInput} style={{width: "calc(100% - 70px)", border: "none", marginLeft: "10px"}} />
+            <input onChange={e => setInputText(e.target.value)} value={inputText} ref={tInput} style={{ width: "calc(100% - 70px)", border: "none", marginLeft: "10px" }} />
         </form>
     )
 }
--- src/components/computer.js
+++ src/components/computer.js
@@ -1,28 +1,46 @@
+const ColorLookup = { 0: "#0C0C0C", 1: "#0037DA", 2: "#13A10E", 3: "#3A96DD", 4: "#C50F1F", 5: "#881798", 6: "#C19C00", 7: "#CCCCCC", 8: "#767676", 9: "#3B78FF", a: "#16C60C", b: "#61D6D6", c: "#E74856", d: "#B4009E", e: "#F9F1A5", f: "#F2F2F2" }
+const helpColor = `Sets the default console foreground and background colors.
+
+COLOR [attr]
+
+  attr        Specifies color attribute of console output
+
+Color attributes are specified by TWO hex digits -- the first
+corresponds to the background; the second the foreground.  Each digit
+can be any of the following values:
+
+    0 = Black       8 = Gray
+    1 = Blue        9 = Light Blue
+    2 = Green       A = Light Green
+    3 = Aqua        B = Light Aqua
+    4 = Red         C = Light Red
+    5 = Purple      D = Light Purple
+    6 = Yellow      E = Light Yellow
+    7 = White       F = Bright White`
+
+
 const Computer = (text, print) => {
     const compute = () => {
-
         const words = text.toLowerCase().indexOf(' ') > 0 ? text.split(' ') : [text];
-
         switch (words[0]) {
             case "help":
                 switch (words[1]) {
                     case "color":
-                        return "help color..."
+                        return helpColor
                     default:
                         return `For more information on a specific command, type HELP command-name
-color           here is something
-hello           say hello human`
+color           Sets the default console foreground and background colors.`
                 }
 
             case "color":
-                if (words[1]) {
-                    if (words[1].match(/^[0-9a-f]{2}$/)) {
-                        return "Regex is happy, Update the colors"
-                    } else {
-                        return "That wasnt a valid color parameter..."
-                    }
+                if (words[1] && words[1].match(/^[0-9a-f]{2}$/)) {
+                    document.documentElement.style.setProperty('--terminal_bg', ColorLookup[words[1][0]]);
+                    document.documentElement.style.setProperty('--terminal_fg', ColorLookup[words[1][1]]);
+                    return null
                 } else {
-                    return "default"
+                    document.documentElement.style.setProperty('--terminal_bg', ColorLookup["0"]);
+                    document.documentElement.style.setProperty('--terminal_fg', ColorLookup["f"]);
+                    return helpColor
                 }
 
             default:
@@ -31,9 +49,10 @@ hello           say hello human`
     }
 
 
-
-
-    print(lines => [...lines, { text: compute(), by: 0 }]);
+    const response = compute();
+    if (response) {
+        print(lines => [...lines, { text: compute(), by: 0 }]);
+    }
 }
 
 export default Computer;
