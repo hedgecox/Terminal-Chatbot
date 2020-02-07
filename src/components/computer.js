fbbae7446875dca5528dc10490742fcae8c926c0
Luke Hedgecox <luke@emotion-robotics.com>
2020-02-07 23:24:42 +0000

    Updated help commands

--- src/components/computer.js
+++ src/components/computer.js
@@ -1,20 +1,20 @@
 const ColorLookup = {
-	0: "#0C0C0C",
-	1: "#0037DA",
-	2: "#13A10E",
-	3: "#3A96DD",
-	4: "#C50F1F",
-	5: "#881798",
-	6: "#C19C00",
-	7: "#CCCCCC",
-	8: "#767676",
-	9: "#3B78FF",
-	a: "#16C60C",
-	b: "#61D6D6",
-	c: "#E74856",
-	d: "#B4009E",
-	e: "#F9F1A5",
-	f: "#F2F2F2"
+  0: "#0C0C0C",
+  1: "#0037DA",
+  2: "#13A10E",
+  3: "#3A96DD",
+  4: "#C50F1F",
+  5: "#881798",
+  6: "#C19C00",
+  7: "#CCCCCC",
+  8: "#767676",
+  9: "#3B78FF",
+  a: "#16C60C",
+  b: "#61D6D6",
+  c: "#E74856",
+  d: "#B4009E",
+  e: "#F9F1A5",
+  f: "#F2F2F2"
 };
 const helpColor = `Sets the default console foreground and background colors.
 
@@ -36,48 +36,56 @@ can be any of the following values:
     7 = White       F = Bright White`;
 
 const Computer = (text, setLines, setChatbotActive) => {
-	const compute = () => {
-		const lowerText = text.toLowerCase();
-		const words = lowerText.indexOf(" ") > 0 ? lowerText.split(" ") : [lowerText];
-		switch (words[0]) {
-			case "help":
-				switch (words[1]) {
-					case "color":
-						return helpColor;
-					default:
-						return `For more information on a specific command, type HELP command-name
+  const compute = () => {
+    const lowerText = text.toLowerCase();
+    const words =
+      lowerText.indexOf(" ") > 0 ? lowerText.split(" ") : [lowerText];
+    switch (words[0]) {
+      case "help":
+        switch (words[1]) {
+          case "color":
+            return helpColor;
+          default:
+            return `For more information on a specific command, type HELP command-name
 color           Sets the default console foreground and background colors.
-cls             Clear the screen`;
-				}
+cls             Clear the screen
+chat			Activate chatbot`;
+        }
 
-			case "color":
-				if (words[1] && words[1].match(/^[0-9a-f]{2}$/)) {
-					document.documentElement.style.setProperty(
-						"--terminal_bg",
-						ColorLookup[words[1][0]]
-					);
-					document.documentElement.style.setProperty(
-						"--terminal_fg",
-						ColorLookup[words[1][1]]
-					);
-					return null;
-				} else {
-					document.documentElement.style.setProperty("--terminal_bg", ColorLookup["0"]);
-					document.documentElement.style.setProperty("--terminal_fg", ColorLookup["f"]);
-					return helpColor;
-				}
-			case "chat":
-				setChatbotActive(true);
-				return "Activating Chatbot, Press CTRL + C to exit";
-			default:
-				return `'${text}' is not recognized as an internal or external command, operable program or batch file.`;
-		}
-	};
+      case "color":
+        if (words[1] && words[1].match(/^[0-9a-f]{2}$/)) {
+          document.documentElement.style.setProperty(
+            "--terminal_bg",
+            ColorLookup[words[1][0]]
+          );
+          document.documentElement.style.setProperty(
+            "--terminal_fg",
+            ColorLookup[words[1][1]]
+          );
+          return null;
+        } else {
+          document.documentElement.style.setProperty(
+            "--terminal_bg",
+            ColorLookup["0"]
+          );
+          document.documentElement.style.setProperty(
+            "--terminal_fg",
+            ColorLookup["f"]
+          );
+          return helpColor;
+        }
+      case "chat":
+        setChatbotActive(true);
+        return "Activating Chatbot, Press CTRL + C to exit";
+      default:
+        return `'${text}' is not recognized as an internal or external command, operable program or batch file. Type 'Help' or 'Chat' to get started`;
+    }
+  };
 
-	const response = compute();
-	if (response) {
-		setLines(lines => [...lines, { text: response, by: 0 }]);
-	}
+  const response = compute();
+  if (response) {
+    setLines(lines => [...lines, { text: response, by: 0 }]);
+  }
 };
 
 export default Computer;
